702a6352ad3c6a6380319ae1506e5c1751b5f1d7
Joseph Di Pasquale <joseph@josephdp.com>
2017-06-18 21:06:28 -0400

    Initial Commit

---
+++ Up and GO for Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Up and GO for Mac
+//
+//  Created by Joseph Di Pasquale on 2017-06-18.
+//  Copyright © 2017 SiteWiz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
